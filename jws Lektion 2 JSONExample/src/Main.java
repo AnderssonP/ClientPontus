9786a74ddfe670745c024f4282f0fae8d8f9bf53
Pontus <pontus.andersson@gritacademy.com>
2023-03-09 10:27:22 +0100

    Skapat ett JSON Object och skrivit och hämtat data via den.

--- jws Lektion 2 JSONExample/src/Main.java
+++ jws Lektion 2 JSONExample/src/Main.java
@@ -1,8 +1,20 @@
+import org.json.simple.JSONObject;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
         System.out.println("This is an example!");
 
+        //skapa ett JSON Object
+        JSONObject jsonOb = new JSONObject();
+
+        //spara värden i JSON object
+        jsonOb.put("namn","Pontus");
+        jsonOb.put("age",24);
+
+        //Skriva ut värden
+        System.out.println("Mitt namn är "+jsonOb.get("namn"));
+        System.out.println("jag är "+ jsonOb.get("age")+" år gammal");
     }
 }
