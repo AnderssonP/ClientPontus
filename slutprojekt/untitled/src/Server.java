342a3438cbed6da18b96280aad8a07a66745881c
Pontus <pontus.andersson@gritacademy.com>
2023-03-16 09:22:14 +0100

    kod added

---
+++ slutprojekt/untitled/src/Server.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main (String[] args){
+        Socket socket = null;
+        InputStreamReader inputStreamReader = null;
+        OutputStreamWriter outputStreamWriter = null;
+        BufferedReader bufferedReader = null;
+        BufferedWriter bufferedWriter = null;
+        ServerSocket serverSocket = null;
+
+        try{
+            serverSocket = new ServerSocket(4321);
+        } catch (Exception e){
+            System.out.println(e);
+            return;
+        }
+    }
+}
